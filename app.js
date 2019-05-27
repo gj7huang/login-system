59bf0b04376189e3dedbb042809af02d3677aafa
jayHuang0728 <open159259@gmail.com>
2019-05-27 17:32:31 -0400

    third commit

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ import bcrypt from 'bcrypt';
 
 
 const app = express();
-// const pool = mysql.createPool(db.mysql);
-const [PORT = 3000, HOST = `localhost`] = [process.env.PORT, process.env.CUSTOMVAR_HOSTNAME];
+const [PORT = 8080, HOST = `localhost`] = [process.env.PORT, process.env.CUSTOMVAR_HOSTNAME];
 
 
 app.engine('html', require('ejs').renderFile);
@@ -25,109 +24,115 @@ app.get('/*', function (req, res, next) {
     next();
 });
 
-// app.use('/', express.static(__dirname + '/views'));
-// // app.disable("etag");
+app.use('/', express.static(__dirname + '/views'));
+// app.disable("etag");
 
-// // show index
-// app.route('/')
-//     .get((req, res) => {
-//         res.render('index');
-//     });
+
+app.route('/')
+    .get((req, res) => {
+        res.render('index');
+    });
 
 app.get('/showAllUser', (req, res) => {
-    User.find({}).exec((err, user) => {
-        if(err) {
-            res.send(500, err);
-        } else {
-            res.send(user.map(el => { 
-                return { email: el.email, student_id: el.student_id, gender: el.gender }
-            }));
-        }
-        
+    User.find({}).exec()
+    .then((user) => {
+        res.send(user.map(el => { 
+            return { email: el.email, student_id: el.student_id, gender: el.gender }
+        }));
+    })
+    .catch((err) => {
+        res.send(500, err);
     })
 })
 
 app.post('/register', (req, res) => {
     let { email, student_id, password, gender} = req.body;
-    //console.log(req.body);
+    // let email = req.param('email');
+    // let student_id = req.param('student_id');
+    // let password = req.param('password');
+    // let gender = req.param('gender');
+    let token;
 
-    bcrypt.hash(password, 10).then((hash) => {
-
-        let newUser = new User({ 
-            email: email, 
-            student_id: student_id, 
-            password: password, 
-            token: hash,
-            gender: gender
-        });
-        //console.log(hash);
-        newUser.save(err => {
-            if (err) {
-                if (err.code === 11000) {
-                    res.status(400).send({
-                        code: 400,
-                        errorMsg: 'This account already exists...'
-                    });
-                } else {
-                    res.status(400).send({
-                        code: 400,
-                        errorMsg: err.message
-                    });
-                }
-            } else {
-                res.send({
-                    code: 200,
-                    successMsg: 'register successfully!',
-                    user: {
-                        token: hash,
-                    }
-                });
+    User.findOne({'$or': [{ email: email }, { student_id: student_id }]})
+    .then((user) => {
+        if(user){
+            res.status(400).send({
+                code: 400,
+                errorMsg: 'This account already exists...'
+            });
+         } else {
+            token = bcrypt.hashSync(password, 10);
+            // console.log(token, password)
+            let newUser = new User({ 
+                email: email, 
+                student_id: student_id, 
+                password: password, 
+                token: token,
+                gender: gender
+            });
+            return newUser.save()
+        }
+    })
+    .then((saved) => {
+        res.send({
+            code: 200,
+            successMsg: 'register successfully!',
+            user: {
+                token: token
             }
-        })
+        });
+    })
+    .catch((err) => {
+        res.status(400).send({
+            code: 400,
+            errorMsg: `${email ? '': 'email '}${student_id ? '': 'student_id '}${password ? '': 'password '}${gender ? '': 'gender '}must be provided...`
+        });
+    })
+    .catch((err) => {
+        // console.log(err);
     })
 });
 app.post('/login', (req, res) => {
     let { email, password } = req.body;
-    if(email && password) {
-        User.findOne({
-            email: email,
-            //student_id: student_id
-        }).exec((err, user) => {
-            if(!user) {
-                console.log(err);
-                res.status(400).send({
-                    code: 400,
-                    errorMsg: 'Account doesn\'t exist...'
-                });
-            } else {
-                if(!bcrypt.compareSync(password, user.token)) {
-                    res.status(400).send({
-                        code: 400,
-                        errorMsg: 'Password is wrong...'
-                    });
-                } else {
-                    let { _id, email, student_id, token, gender } = user;
-                    res.status(200).send({
-                        code: 200,
-                        successMsg: 'Login !',
-                        user: { 
-                            id: _id,
-                            email: email, 
-                            student_id: student_id, 
-                            token: token,
-                            gender: gender
-                        }
-                    });
-                }
-            }
-        })
-    } else {
-        res.status(400).send({
-            code: 400,
-            errorMsg: 'Email, Password must be provided...'
-        });
-    }
+    // let email = req.param('email');
+    // let password = req.param('password');
     
+    User
+    .findOne({email: email})
+    // .exec()
+    .then((user) => {
+         if(!bcrypt.compareSync(password, user.token)) {
+            res.status(400).send({
+                code: 400,
+                errorMsg: 'Password is wrong...'
+            });
+        } else {
+            let { _id, email, student_id, token, gender } = user;
+            res.status(200).send({
+                code: 200,
+                successMsg: 'Login !',
+                user: { 
+                    id: _id,
+                    email: email, 
+                    student_id: student_id, 
+                    token: token,
+                    gender: gender
+                }
+            });
+        }
+    })
+    .catch((err) => {
+        if (email) 
+            res.status(400).send({
+                code: 400,
+                errorMsg: 'This account doesn\'t exist...'
+            });
+        else
+            res.status(400).send({
+                code: 400,
+                errorMsg: 'email must be provided...'
+            });
+    })
 });
 
 
---
+++ views/js/all.js
@@ -0,0 +1,111 @@
+let userList = [];
+let loginUserIndex = -1;
+
+function initFormAnimation() {
+    $('.toggle-register').click(function () {
+        $(this).addClass('active');
+        $('.toggle-login').removeClass('active');
+        $('.login-body').slideUp("slow");
+        $('.register-body').delay(625).slideDown("nomal");
+
+        $('#login-email').val('');
+        $('#login-pwd').val('');
+        $('#login-msg').text('');
+    });
+
+    $('.toggle-login').click(function () {
+        $(this).addClass('active');
+        $('.toggle-register').removeClass('active');
+        $('.register-body').slideUp("slow");
+        $('.login-body').delay(625).slideDown("nomal");
+
+        $('#register-student-id').val('');
+        $('#register-email').val('');
+        $('#register-pwd').val('');
+        $('#gender-select').val('-1');
+        $('#register-msg').text('');
+    });
+
+    $('#registered').click(function () {
+        $('.toggle-login').click();
+    });
+}
+
+function getAllUser() {
+    $('#all-user-info').empty();
+    $.ajax({
+        url: '/showAllUser',
+        method: "GET",
+    }).then(res => {
+        userList = res;
+        res.map((el, index) => {
+            $('#all-user-info').append(`
+                <div class="user-info" id="item-${index}">
+                    <h3>${el.student_id}</h3>
+                    <ul>
+                        <il>Email: ${el.email}</il>
+                        <il>Gender: ${el.gender}</il>
+                    </ul>
+                </div>
+            `);
+        })
+    });
+}
+
+function readyFunc() {
+    initFormAnimation();
+    getAllUser();
+
+    $('#btn-login').click((event) => {
+        let email = $('#login-email').val();
+        let pwd = $('#login-pwd').val();
+        $.ajax({
+            url: '/login',
+            method: "POST",
+            data: {
+                email: email,
+                password: pwd
+             }
+        }).then(res => {
+            $('#all-user-info > div').removeClass('user-active');
+            userList.map((el, index) => {
+                // console.log(el.student_id === res.user.student_id);
+                if (el.student_id === res.user.student_id) {
+                    loginUserIndex = index;
+                    $(`#item-${index}`).addClass('user-active');
+                }
+            })
+            $('#login-msg').text(res.successMsg);
+        }).catch(err => {
+            $('#all-user-info > div').removeClass('user-active');
+            // console.log(err);
+            $('#login-msg').text(err.responseJSON.errorMsg);
+        });
+    })
+
+    $('#btn-register').click((event) => {
+        let student_id = $('#register-student-id').val();
+        let email = $('#register-email').val();
+        let pwd = $('#register-pwd').val();
+        let gender = $('#gender-select').val();
+
+        // console.log(`${student_id}-${email}-${pwd}-${gender}`)
+        $.ajax({
+            url: '/register',
+            method: "POST",
+            data: {
+                student_id: student_id,
+                email: email,
+                password: pwd,
+                gender: gender,
+            }
+        }).then(res => {
+            getAllUser();
+            $('#register-msg').text(res.successMsg);
+        }).catch(err => {
+            $('#register-msg').text(err.responseJSON.errorMsg);
+        });
+    })
+}
+
+$(document).ready(readyFunc);
